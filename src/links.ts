46f991eafdb14c1ae87f9c40717c4f1e89d05451
Oleg Shparber <trollixx@gmail.com>
2026-06-04 17:10:11 +0300

    fix: point /l/discord at discord.com to skip a redirect

--- src/links.ts
+++ src/links.ts
@@ -2,7 +2,7 @@ export const linkMap: Record<string, string> = {
     contact: "https://zealdocs.org/contact.html",
     download: "https://zealdocs.org/download.html",
 
-    discord: "https://discord.gg/KKX6HNH",
+    discord: "https://discord.com/invite/KKX6HNH",
     github: "https://github.com/zealdocs/zeal",
     gitter: "https://gitter.im/zealdocs/zeal",
     twitter: "https://twitter.com/zealdocs",
